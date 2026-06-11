e05c0a25fbe80668fd273d32502d2b19905f79e5
emiliano_osuna <emilianingo2@gmail.com>
2026-06-11 13:20:33 -0700

    fix: use implicit OAuth flow for PWA, PKCE for native

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -16,7 +16,7 @@ export const supabase = createClient(
   {
     auth: {
       detectSessionInUrl: !isNative,
-      flowType: 'pkce',
+      flowType: isNative ? 'pkce' : 'implicit',
     },
   }
 )
